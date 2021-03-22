33d7d30e95e82b1bc12f42b478d44491d00dde7f
trophyguarder <61644109+trophyguarder@users.noreply.github.com>
2021-03-22 17:53:08 -0400

    updated theme

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,264 +1,297 @@
-/* ===================================================================
-    Author          : KeenDevs
-    Template Name   : Klaud Hosting Domain HTML Template
-    Version         : 1.1
-* ================================================================= */
+// Index of jQuery Active Code
 
-(function($) {
-    "use strict";
+// :: 1.0 PRELOADER ACTIVE CODE
+// :: 2.0 NAVIGATION MENU ACTIVE CODE
+// :: 3.0 SCROLL TO TOP ACTIVE CODE
+// :: 4.0 SCROLL LINK ACTIVE CODE
+// :: 5.0 SMOOTH SCROLLING ACTIVE CODE
+// :: 6.0 AOS ACTIVE CODE
+// :: 7.0 AOS ACTIVE CODE
+// :: 8.0 PREVENT DEFAULT ACTIVE CODE
+// :: 9.0 COUNTERUP ACTIVE CODE
+// :: 10.0 FANCYBOX VIDEO POPUP ACTIVE CODE
+// :: 11.0 FEATURES SLIDER ACTIVE CODE
+// :: 12.0 TESTIMONIALS ACTIVE CODE
+// :: 13.0 APP SCREENSHOTS ACTIVE CODE
+// :: 14.0 CONTACT FORM ACTIVE CODE
 
-    $(document).on('ready', function() {
+(function ($) {
+    'use strict';
 
+    var $window = $(window);
+    var zero = 0;
 
-    $(window).on('load', function() {
-        // Animate loader off screen
-        $(".pre-loader").delay(500).fadeOut();
-    });
-
-    // Domain Placehoder
-    var placehoder = document.querySelector('.search-box-inner input');
-    var pText = placehoder.placeholder;
-
-    const sp = superplaceholder({
-        el: placehoder,
-        sentences: [pText],
-        options: {
-        loop: true
-      }
-    });
-    
-    sp.start();
-
-    
-    AOS.init();
-
-    $('.package-control li').on('click', function(){
-      $(".package-control li").removeClass('active');      
-      $(this).addClass('active');      
-    });
-
-
-    $('.card-header .btn').on('click', function(){
-      $(".accordion-wrap .card").not($(this).closest('.card')).removeClass('cbg');
-      $(this).closest('.card').toggleClass('cbg');
-    });
-
-
-    // $('.domain-list').select2();
-  
-
-
-    /*========================================
-     # Hero SLide Carousel  - Home Page 3
-     ========================================*/ 
-
-      $('.hero-slider-active').slick({
-        slidesToShow: 1,
-        dots: true,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,        
-      });
-    
-
-    /*========================================
-     # Testomminal Carousel 
-     ========================================*/  
-
-    $('.testimonial-list').slick({
-        slidesToShow: 3,
-        dots: true,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        centerPadding: '0px',
-        autoplaySpeed: 3000,
-        centerMode: true,
-        responsive: [
-          {
-            breakpoint: 1191,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          
-        ]
-
-    });    
-
-    /* =============================================
-        # Magnific popup init
-     ===============================================*/
-    $(".popup-link").magnificPopup({
-        type: 'image',
-        fixedContentPos: false
-    });
-
-    $(".popup-gallery").magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true,
-            fixedContentPos: false
-        },
-        // other options
-    });
-
-    $(".popup-video, .popup-vimeo, .popup-gmaps").magnificPopup({
-        type: "iframe",
-        mainClass: "mfp-fade",
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-
-    /*==================================
-     # Meanmenu Active - Mobile Menu 
-     ==================================*/
-    $('#responsive-menu').meanmenu({
-        meanMenuContainer: '.responsive-menu',
-        meanScreenWidth: "991"
-    });
-
-
-    /*==========================
-       Scroll To Up Init
-    ============================*/
-    $.scrollUp({
-      scrollName: 'scrollUp', // Element ID
-      topDistance: '1110', // Distance from top before showing element (px)
-      topSpeed: 2000, // Speed back to top (ms)
-      animation: 'slide', // Fade, slide, none
-      animationInSpeed: 300, // Animation in speed (ms)
-      animationOutSpeed: 300, // Animation out speed (ms)
-      scrollText: '<i class="fal fa-angle-up"></i>', // Text for element
-      activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-    });
-
-    // data - background
-    $("[data-background]").each(function () {
-        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-    });
-
-
-
-    //# Smooth Scroll
-    $('#responsive-menu a').on('click', function(event) {
-        var $anchor = $(this);
-        var headerH = '85';
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-
-    // Sticky Menu
-    $(window).scroll(function(){
-      var Width = $(document).width();
-
-        if($("body").scrollTop() > 100 || $("html").scrollTop() > 100 ) {
-          if (Width > 767 ) {
-            $("header").addClass("sticky");
-          }
-        } else {
-            $("header").removeClass("sticky");
+    // :: 1.0 PRELOADER ACTIVE CODE
+    $(window).on("load", function () {
+        $('.preloader-wapper').addClass('loaded');
+        if ($('.preloader-wapper').hasClass('loaded')) {
+            $('.preloader-main').delay(1200).queue(function () {
+                $(this).remove();
+            });
         }
     });
-      
 
-     // CouterUp 
-    $('.price span').each(function () {
-       var size = $(this).text().split(".")[1] ? $(this).text().split(".")[1].length : 0;
-       $(this).prop('Counter', 0).animate({
-          Counter: $(this).text()
-       }, {
-          duration: 3000,
-          step: function (func) {
-             $(this).text(parseFloat(func).toFixed(size));
-          }
-       });
+    // :: 2.0 NAVIGATION MENU ACTIVE CODE
+    // dropdown for mobile
+    $(document).ready(function () {
+        checkWidth(true);
+        $(window).resize(function () {
+            checkWidth(false);
+        });
     });
 
+    function checkWidth(init) {
+        // If browser resized, check width again 
+        if ($(window).width() <= 991) {
+            $('.dropdown-submenu a').on("click", function (e) {
+                $(this).next('ul').toggle();
+                e.stopPropagation();
+            });
+        }
+    }
+    function navMenu() {
 
+        // MAIN MENU TOGGLER ICON (MOBILE SITE ONLY)
+        $('[data-toggle="navbarToggler"]').click(function () {
+            $('.navbar').toggleClass('active');
+            $('body').toggleClass('canvas-open');
+        });
+        // MAIN MENU TOGGLER ICON
+        $('.navbar-toggler').click(function () {
+            $('.navbar-toggler-icon').toggleClass('active');
+        });
 
+        // NAVBAR STICKY
+        var $stickyNav = $(".navbar-sticky");
 
+        $(window).on("scroll load", function () {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 120) {
+                $stickyNav.addClass("navbar-sticky-moved-up");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-moved-up");
+            }
+            // apply transition
+            if (scroll >= 250) {
+                $stickyNav.addClass("navbar-sticky-transitioned");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-transitioned");
+            }
+            // sticky on
+            if (scroll >= 500) {
+                $stickyNav.addClass("navbar-sticky-on");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-on");
+            }
 
-  /*** Select Field Custom */
-    $('select').each(function(){
-      var $this = $(this), numberOfOptions = $(this).children('option').length;
-      var overflow = numberOfOptions > 5 ? 'overflow-y' : '';
-      $this.addClass('select-hidden'); 
-      $this.wrap('<div class="select"></div>');
-      $this.after('<div class="select-styled"></div>');
+        });
+    }
+    navMenu();
 
-      var $styledSelect = $this.next('div.select-styled');
-      $styledSelect.text($this.children('option').eq(0).text());
-    
-      var $list = $('<ul />', {
-          'class': 'select-options'
-      }).insertAfter($styledSelect);
-    
-      for (var i = 0; i < numberOfOptions; i++) {
-          $('<li />', {
-              text: $this.children('option').eq(i).text(),
-              rel: $this.children('option').eq(i).val()
-          }).appendTo($list);
-      }
-    
-      var $listItems = $list.children('li');
-    
-      $styledSelect.click(function(e) {
-          e.stopPropagation();
-          $('div.select-styled.active').not(this).each(function(){
-              $(this).removeClass('active').next('ul.select-options').hide();
-          });
-          $(this).toggleClass('active').next('ul.select-options').addClass(overflow).toggle();
-      });
-    
-      $listItems.click(function(e) {
-          e.stopPropagation();
-          $styledSelect.text($(this).text()).removeClass('active');
-          $this.val($(this).attr('rel'));
-          $('select option').removeAttr('selected');
-        $('select option[value="'+$(this).attr('rel')+'"]').attr('selected','selected');
-        // Only Woo Orderby
-        if ($this.hasClass('orderby')) {
-          $(this).closest( 'form' ).submit();
-      }
-          $list.hide();
-      });
-    
-      $(document).click(function() {
-          $styledSelect.removeClass('active');
-          $list.hide();
-      });
+    // :: 3.0 SCROLL TO TOP ACTIVE CODE
+    var offset = 300;
+    var duration = 500;
 
-  });
+    $window.on('scroll', function () {
+        if ($(this).scrollTop() > offset) {
+            $("#scrollUp").fadeIn(duration);
+        } else {
+            $("#scrollUp").fadeOut(duration);
+        }
+    });
 
+    $("#scrollUp").on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, duration);
+    });
 
+    // :: 4.0 SCROLL LINK ACTIVE CODE
+    var scrollLink = $('.scroll');
 
+    // :: 5.0 SMOOTH SCROLLING ACTIVE CODE
+    scrollLink.on('click', function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
 
+    // :: 6.0 AOS ACTIVE CODE
+    AOS.init();
 
+    // :: 7.0 WOW ACTIVE CODE
+    new WOW().init();
 
+    // :: 8.0 PREVENT DEFAULT ACTIVE CODE
+    $("a[href='#']").on('click', function ($) {
+        $.preventDefault();
+    });
 
-    }); // end document ready function
-})(jQuery); // End jQuery
+    // :: 9.0 COUNTERUP ACTIVE CODE
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
 
+    // :: 10.0 FANCYBOX VIDEO POPUP ACTIVE CODE
+    $(".play-btn").fancybox({
+        animationEffect: "zoom-in-out",
+        transitionEffect: "circular",
+        maxWidth: 800,
+        maxHeight: 600,
+        youtube: {
+            controls: 0
+        }
+    });
+
+    // :: 11.0 FEATURES SLIDER ACTIVE CODE
+    $('.features-slider.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 1500,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dotsContainer: '.features-content'
+    });
+
+    // :: 12.0 TESTIMONIALS ACTIVE CODE
+    var testimonialSlider = $('.testimonials');
+    testimonialSlider.owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+        smartSpeed: 500,
+        autoplay: true,
+        loop: true,
+        mouseDrag: true,
+        touchDrag: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            768: {
+                items: 1
+            }
+        }
+    });
+
+    testimonialSlider.on("translate.owl.carousel", function () {
+        $(".single-testimonial img, .single-testimonial-thumb img, .client-rating").removeClass("animated zoomIn").css("opacity", "0");
+    });
+    testimonialSlider.on("translated.owl.carousel", function () {
+        $(".single-testimonial img, .single-testimonial-thumb img, .client-rating").addClass("animated zoomIn").css("opacity", "1");
+    });
+
+    // :: 13.0 BRANDING SLIDER ACTIVE CODE
+    $('.branding-slider.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        smartSpeed: 2000,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            768: {
+                items: 4
+            },
+            992: {
+                items: 5
+            }
+        }
+    });
+
+    // :: 14.0 APP SCREENSHOTS ACTIVE CODE
+    $('.app-screenshots').slick({
+        dots: true,
+        arrows: false,
+        speed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+          ]
+    });
+
+    // :: 15.0 CONTACT FORM ACTIVE CODE
+    // Get the form.
+    var form = $('#contact-form');
+    // Get the messages div.
+    var formMessages = $('.form-message');
+    // Set up an event listener for the contact form.
+    $(form).submit(function (e) {
+        // Stop the browser from submitting the form.
+        e.preventDefault();
+        // Serialize the form data.
+        var formData = $(form).serialize();
+        // Submit the form using AJAX.
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+            .done(function (response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages).removeClass('error');
+                $(formMessages).addClass('success');
+
+                // Set the message text.
+                $(formMessages).text(response);
+
+                // Clear the form.
+                $('#contact-form input,#contact-form textarea').val('');
+            })
+            .fail(function (data) {
+                // Make sure that the formMessages div has the 'error' class.
+                $(formMessages).removeClass('success');
+                $(formMessages).addClass('error');
+
+                // Set the message text.
+                if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+                } else {
+                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                }
+            });
+    });
+
+}(jQuery));
